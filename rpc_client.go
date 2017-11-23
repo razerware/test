4371fa0169674f94b6e474de1ec8d977115570d5
razerware <lizhengyin1115@163.com>
2017-11-23 17:43:46 +0800

    gg

--- rpc_client.go
+++ rpc_client.go
@@ -7,13 +7,11 @@ import (
 	"net"
 )
 
-type args struct {
-	A, B int
+type MonitorStats struct {
+	CpuPercent string
+	MemPercent string
 }
 
-type quotient struct {
-	Quo, Rem int
-}
 
 func main() {
 	address, err := net.ResolveTCPAddr("tcp", "127.0.0.1:4200")
@@ -26,22 +24,16 @@ func main() {
 	client := rpc.NewClient(conn)
 	defer client.Close()
 
-	args := &args{7, 8}
-	// Synchronous call
-	var reply int
-	err = client.Call("Arith.Multiply", args, &reply)
-	if err != nil {
-		log.Fatal("arith error:", err)
-	}
-	fmt.Printf("Arith: %d*%d=%d\n", args.A, args.B, reply)
+	args := &MonitorStats{"7%", "8%"}
 
-	// Asynchronous call
-	quotient := new(quotient)
-	divCall := client.Go("Arith.Divide", args, quotient, nil)
-	replyCall := <-divCall.Done // will be equal to divCall
-	if replyCall.Error != nil {
-		log.Fatal("arith error:", replyCall.Error)
+	res:=false
+	for{
+		lzycall:=client.Go("MonitorStats.Collect",args,&res,nil)
+		relzycall:=<-lzycall.Done
+		if relzycall.Error != nil {
+			log.Fatal("arith error:", relzycall.Error)
+		}
+		fmt.Printf("lzy %v",res)
 	}
-	fmt.Printf("Arith: %d/%d=%d...%d", args.A, args.B, quotient.Quo, quotient.Rem)
-	// check errors, print, etc.
+
 }
